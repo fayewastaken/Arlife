f07cb0c3a33cd5df633fe05f8975291b5ec05f41
fayewastaken <68054377+fayewastaken@users.noreply.github.com>
2021-03-18 01:09:41 +0700

    Adding Status Bar system

--- GameManager.cs
+++ GameManager.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float Money;
+    public float Age;
+
+    public float Happiness;
+    public float Health;
+    public float Smarts;
+    public float Looks;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) {
             Application.Quit();
         }
+    }
+
+    public void StatusRandomRange()
+    {
+        Happiness = Mathf.Round(Random.Range(40f, 100f));
+        Health = Mathf.Round(Random.Range(60f, 100f));
+        Smarts = Mathf.Round(Random.Range(60f, 100f));
+        Looks = Mathf.Round(Random.Range(0f, 100f));
     }
 }
